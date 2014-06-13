105b279befad58021543d65206413676c00fb235
Gustavo Velasques Dreier <gustavodreier@gmail.com>
2014-06-13 17:05:11 -0300

    adicionado algoritmo dijkstra e funcoes para conseguir executar o algoritmo. Alterado classe de teste para testar o algoritmo

--- src/estruturas/tgrafo/Grafo.java
+++ src/estruturas/tgrafo/Grafo.java
@@ -23,11 +23,23 @@ public class Grafo {
 		//adiciona um novo nodo quando não conhece a chave
 		//neste caso as chaves devem ficar sequencias (1, 2, 3, 4)
 		Nodo nodoVertice = new Nodo(valor);
-		int key = listaVertice.size() - 1;
+		int key = listaVertice.size();
 		listaVertice.add(new Vertice(key, nodoVertice));
 	}
+	
+	
+	
 	public int getOrdem() {
 		//retorna a ordem de um grafo (numero de elementos)
-		return listaVertice.size() - 1;
+		return listaVertice.size();
 	}
+	
+	public Vertice getVertice(int key) {
+		for (Vertice v : listaVertice) {
+			if(v.getKey() == key)
+				return v;
+		}
+		return null;
+	}
+	
 }
--- src/principal/Aplicacao.java
+++ src/principal/Aplicacao.java
@@ -1,11 +1,16 @@
 package principal;
 
 import estruturas.Stack;
+import estruturas.tgrafo.Aresta;
+import estruturas.tgrafo.Grafo;
+import algoritmo.*;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
 		Stack pilha = new Stack(1);
 		
 		pilha.push(1);
@@ -16,6 +21,34 @@ public class Aplicacao {
 		System.out.println(pilha.pop());
 		System.out.println(pilha.pop());
 		System.out.println(pilha.pop());
+		*/
+		Grafo g = new Grafo();
+		//adiciona os vertices ao grafo
+		g.addVertice(1.0, 1);
+		g.addVertice(2.0, 2);
+		g.addVertice(3.0, 3);
+		
+		//adiciona as arestas aos vertices
+		Aresta a1 = new Aresta(1, g.getVertice(1), g.getVertice(2));
+		Aresta a2 = new Aresta(1, g.getVertice(2), g.getVertice(3));
+		Aresta a3 = new Aresta(3, g.getVertice(1), g.getVertice(3));
+		
+		//adiciona as arestas aos vertices
+		g.getVertice(1).addAresta(a1);
+		g.getVertice(1).addAresta(a3);
+		g.getVertice(2).addAresta(a2);
+		g.getVertice(2).addAresta(a1);
+		g.getVertice(3).addAresta(a2);
+		g.getVertice(3).addAresta(a3);
+		
+		int[][] melhorCaminho = new int[g.getOrdem()][2]; 
+		melhorCaminho = new Dijkstra().shortPath(g.getVertice(1), g.getOrdem());
+		
+		for (int i = 0; i < g.getOrdem(); i++) {
+			int distancia = melhorCaminho[i][0];
+			int anterior = melhorCaminho[i][1];
+			System.out.println("Chave: "+(i + 1)+"\tdistancia: "+distancia+"\tanterior: "+anterior+"\n");
+		}
 	}
 
 }
